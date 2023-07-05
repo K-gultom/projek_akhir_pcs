21a85b15c8e30be2d1fb6fb6c81777aae37c7850
K-gultom <yehezkielgg28@gmail.com>
2023-07-05 18:29:00 +0700

    commit

--- projek_pcs/lib/Screen/home.dart
+++ projek_pcs/lib/Screen/home.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:projek_pcs/Screen/bottom_navbar.dart';
 import 'package:projek_pcs/Screen/order.dart';
 import 'package:http/http.dart' as http;
 
@@ -96,7 +95,37 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
         
-      bottomNavigationBar: Bottom_navbar(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Riwayat Order',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.logout),
+            label: 'Logout',
+          ),
+        ],
+        onTap: (int index) {
+          // Handle navigation based on index
+          if (index == 0) {
+            // Navigate to Home screen
+            Navigator.pushReplacementNamed(context, '/home');
+          } else if (index == 1) {
+            // Navigate to Riwayat Order screen
+            Navigator.pushNamed(context, '/riwayat_order');
+          } else if (index == 2) {
+            // Perform logout
+            Navigator.pushReplacementNamed(context, '/login');
+            // Or you can use Navigator.pushNamedAndRemoveUntil to remove all the routes and navigate to login screen
+            // Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
+          }
+        },
+      ),
     );
   }
 }
